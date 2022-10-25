3c7eec65d807011fc73e9f554cba95f8425ad45f
Octavio Martinez <omartinez1618@gmail.com>
2022-10-25 17:00:34 -0400

    add end point

--- clases/class_organizer_media.php
+++ clases/class_organizer_media.php
@@ -8,5 +8,31 @@ class OrganizerMedia {
 
     public static function deactive(){}
     
-    public static function init(){}
+    public static function init(){
+        add_action( 'rest_api_init', ['OrganizerMedia','register_end_points'] );
+
+        register_taxonomy_for_object_type( 'category', 'attachment' );
+        register_taxonomy_for_object_type( 'post_tag', 'attachment' );
+    }
+
+    public static function register_end_points( ) {
+        register_rest_route( 'medias/v1', '/media', array(
+          'methods' => 'GET',
+          'callback' => ['OrganizerMedia', 'get_media_data'],
+        ) );
+    }
+
+    public static function get_media_data( ) {
+        $query = get_posts( array(
+            'orderby'          => 'date',
+            'order'            => 'DESC',
+            'include'          => array(),
+            'exclude'          => array(),
+            'meta_key'         => '',
+            'meta_value'       => '',
+            'post_type'        => 'attachment',
+            'suppress_filters' => true,
+        ));
+        return $query;
+    }
 }
